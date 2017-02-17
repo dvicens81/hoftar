b5d59199de1c8c70cc4a3710cea4df55e547e645
david_vicens <Erni2016>
2017-02-17 14:35:04 +0100

    First version

---
+++ exemple/README.md
@@ -0,0 +1,2 @@
+1. Run command to python main.py
+2. Enter a array. For example 123456
---
+++ exemple/main/main.py
@@ -0,0 +1,10 @@
+import re
+var = raw_input("Please enter a array (Ex: 123433245): ")
+
+regex = r"123"
+compiled_re = re.compile(regex)
+res = compiled_re.search(var)
+if res is not None:
+    print 'Found it %s ' % var
+else:
+    print 'Not found in %s ' % var
